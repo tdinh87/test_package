86c5b635a62c5950e73f55e6a1d84e71998c5f6d
Tien Dung Dinh <TienDung.Dinh@UGent.be>
2023-01-03 14:26:33 +0100

    initial commit.

---
+++ tdinh87_test_package/test.py
@@ -0,0 +1,14 @@
+import string
+import random
+
+
+def random_name():
+	chars = []
+	for i in range(random.randrange(3, 10)):
+		chars.append(random.choice(string.ascii_letters))
+	return "".join(chars)
+
+
+def run():
+	for i in range(10):
+		print(random_name())
